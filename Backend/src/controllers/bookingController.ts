11a78b59abc899ad8c793d871c27ff8ef885d004
Pascal <pascal.lohscheidt@gmail.com>
2022-11-19 19:21:14 +0100

    chore(backend): adjusted to new db

--- Backend/src/controllers/bookingController.ts
+++ Backend/src/controllers/bookingController.ts
@@ -1,127 +1,132 @@
-import { Request, Response } from "express"
-import { createFaunaClient } from "../graphql/faunaLib"
-import { createBooking, updateBooking, deleteBooking, findBookingById, findBookings } from "../graphql/BookingRequests"
+import { Request, Response } from 'express';
+import { createFaunaClient } from '../graphql/faunaLib';
+import {
+  createBooking,
+  updateBooking,
+  deleteBooking,
+  findBookingById,
+  findBookings,
+} from '../graphql/BookingRequests';
 
 export async function createBookingRequest(req: Request, res: Response) {
-	// Check if body is empty
-	if (Object.keys(req.body).length === 0)
-		return res.status(400).json({
-			error: "Bad Request",
-			message: "The request body is empty",
-		})
+  // Check if body is empty
+  if (Object.keys(req.body).length === 0)
+    return res.status(400).json({
+      error: 'Bad Request',
+      message: 'The request body is empty',
+    });
 
-	try {
-		const body = req.body
-		const client = createFaunaClient("eu")
-		const booking = await client.request(createBooking, {
-			data: {
-				startDate: body.startDate,
-				endDate: body.endDate,
-				distance: body.distance,
-				car: {
-					connect: body.car
-				},
-				startStation: {
-					connect: body.startStation
-				},
-				endStation: {
-					connect: body.endStation
-				}
-			}
-		})
+  try {
+    const body = req.body;
+    const client = createFaunaClient('eu');
+    const booking = await client.request(createBooking, {
+      data: {
+        startDate: body.startDate,
+        endDate: body.endDate,
+        distance: body.distance,
+        car: {
+          connect: body.car,
+        },
+        startPosition: {
+          lat: body.startPosition.lat,
+          long: body.startPosition.long,
+        },
+        incentiveUsed: body.incentiveUsed,
+      },
+    });
 
-		return res.status(201).json(booking)
-	} catch (err) {
-		return res.status(500).json({
-			error: "Internal server error",
-			message: err.message,
-		})
-	}
+    return res.status(201).json(booking);
+  } catch (err) {
+    return res.status(500).json({
+      error: 'Internal server error',
+      message: err.message,
+    });
+  }
 }
 
 export async function updateBookingRequest(req: Request, res: Response) {
-	// Check if body is empty
-	if (Object.keys(req.body).length === 0)
-		return res.status(400).json({
-			error: "Bad Request",
-			message: "The request body is empty",
-		})
+  // Check if body is empty
+  if (Object.keys(req.body).length === 0)
+    return res.status(400).json({
+      error: 'Bad Request',
+      message: 'The request body is empty',
+    });
 
-	try {
-		const bookingId = req.params.id
-		const body = req.body
-		const client = createFaunaClient("eu")
-		const booking = await client.request(updateBooking, {
-			id: bookingId,
-			data: {
-				startDate: body.startDate,
-				endDate: body.endDate,
-				distance: body.distance,
-				car: {
-					connect: body.car
-				},
-				startStation: {
-					connect: body.startStation
-				},
-				endStation: {
-					connect: body.endStation
-				}
-			}
-		})
+  try {
+    const bookingId = req.params.id;
+    const body = req.body;
+    const client = createFaunaClient('eu');
+    const booking = await client.request(updateBooking, {
+      id: bookingId,
+      data: {
+        startDate: body.startDate,
+        endDate: body.endDate,
+        distance: body.distance,
+        car: {
+          connect: body.car,
+        },
+        startStation: {
+          connect: body.startStation,
+        },
+        endStation: {
+          connect: body.endStation,
+        },
+      },
+    });
 
-		return res.status(200).json(booking)
-	} catch (err) {
-		return res.status(500).json({
-			error: "Internal server error",
-			message: err.message,
-		})
-	}
+    return res.status(200).json(booking);
+  } catch (err) {
+    return res.status(500).json({
+      error: 'Internal server error',
+      message: err.message,
+    });
+  }
 }
 
 export async function deleteBookingRequest(req: Request, res: Response) {
-	try {
-		const bookingId = req.params.id
-		const client = createFaunaClient("eu")
-		const booking = await client.request(deleteBooking, {
-			id: bookingId
-		})
+  try {
+    const bookingId = req.params.id;
+    const client = createFaunaClient('eu');
+    const booking = await client.request(deleteBooking, {
+      id: bookingId,
+    });
 
-		return res.status(204).json(true)
-	} catch (err) {
-		return res.status(500).json({
-			error: "Internal server error",
-			message: err.message,
-		})
-	}
+    return res.status(204).json(true);
+  } catch (err) {
+    return res.status(500).json({
+      error: 'Internal server error',
+      message: err.message,
+    });
+  }
 }
 
 export async function getBookingByIdRequest(req: Request, res: Response) {
-	try {
-		const bookingId = req.params.id
-		const client = createFaunaClient("eu")
-		const booking = await client.request(findBookingById, {
-			id: bookingId
-		})
+  try {
+    const bookingId = req.params.id;
+    const client = createFaunaClient('eu');
+    const booking = await client.request(findBookingById, {
+      id: bookingId,
+    });
 
-		return res.status(200).json(booking)
-	} catch (err) {
-		return res.status(500).json({
-			error: "Internal server error",
-			message: err.message,
-		})
-	}
+    return res.status(200).json(booking);
+  } catch (err) {
+    return res.status(500).json({
+      error: 'Internal server error',
+      message: err.message,
+    });
+  }
 }
 
 export async function getBookingsRequest(req: Request, res: Response) {
-	try {
-		const client = createFaunaClient("eu")
-		const bookings = await client.request(findBookings)
+  try {
+    const client = createFaunaClient('eu');
+    const bookings = await client.request(findBookings);
 
-		return res.status(200).json(bookings)
-	} catch (err) {
-		return res.status(500).json({
-			error: "Internal server error",
-			message: err.message,
-		})
-	}
+    return res.status(200).json(bookings);
+  } catch (err) {
+    return res.status(500).json({
+      error: 'Internal server error',
+      message: err.message,
+    });
+  }
 }
--- Backend/src/graphql/bookingRequests.ts
+++ Backend/src/graphql/bookingRequests.ts
@@ -1,52 +1,49 @@
-import { gql } from 'graphql-request'
+import { gql } from 'graphql-request';
 
 export const createBooking = gql`
-  mutation($data: BookingInput!) {
+  mutation ($data: BookingInput!) {
     createBooking(data: $data) {
-        _id,
-        startDate,
-        endDate,
-        distance,
-        car {
-            carMode,
-            name,
-            carType,
-            _id,
-            lastTimeParked,
-            parkedAt {
-                _id,
-                stationType,
-                positionLong,
-                positionLat
-            }
-            currentRange,
-            maximumRange,
-        },
-        startStation {
-                _id,
-                stationType,
-                positionLat,
-                positionLong,
-                chargingSlots
-            },
-        endStation {
-            _id,
-            stationType,
-            positionLat,
-            positionLong,
-            chargingSlots
-        },
+      _id
+      startDate
+      endDate
+      distance
+      incentiveUsed
+      car {
+        carMode
+        name
+        carType
+        _id
+        lastTimeParked
+        parkedAt {
+          _id
+          stationType
+          positionLong
+          positionLat
+        }
+        currentRange
+        maximumRange
+      }
+      startPosition {
+        lat
+        long
+      }
     }
-}
-`
+  }
+`;
 
 export const updateBooking = gql`
-  mutation($id: ID!, $data: BookingInput!) {
+  mutation ($id: ID!, $data: BookingInput!) {
+    mutation($id: ID!, $data: BookingInput!) {
     updateBooking(id: $id, data: $data) {
         _id,
         startDate,
         endDate,
         distance,
+        incentiveUsed,
+        startPosition {
+            lat,
+            long
+        }
         car {
             carMode,
             name,
@@ -62,108 +59,101 @@ export const updateBooking = gql`
             currentRange,
             maximumRange
         },
-        startStation {
-                _id,
-                stationType,
+        waypoints {
+            data {
                 positionLat,
-                positionLong,
-                chargingSlots
+                positionLong
             },
-        endStation {
-            _id,
-            stationType,
-            positionLat,
-            positionLong,
-            chargingSlots
-        },
-    }
-}
-`
-
-export const deleteBooking = gql`
-  mutation($id: ID!) {
-    deleteBooking(id: $id) {
-        _id,
-        startDate,
-        endDate,
-        distance,
-        car {
-            carMode,
-            name,
-            carType,
-            _id,
-            lastTimeParked,
-            parkedAt {
-                _id,
-                stationType,
-                positionLong,
-                positionLat
-            }
-            currentRange,
-            maximumRange
-        },
-        startStation {
-                _id,
-                stationType,
-                positionLat,
-                positionLong,
-                chargingSlots
-            },
-        endStation {
-            _id,
-            stationType,
-            positionLat,
-            positionLong,
-            chargingSlots
-        },
-    }
-}
-`
-
-export const findBookings = gql`
-  query{
-    allBookings {
-        data {
-            _id,
-            startDate,
-            endDate,
-            distance,
-            car {
-                carMode,
-                name,
-                carType,
-                _id,
-                lastTimeParked,
-                parkedAt {
-                    _id,
-                    stationType,
-                    positionLong,
-                    positionLat
-                }
-                currentRange,
-                maximumRange
-            },
-            startStation {
-                _id,
-                stationType,
-                positionLat,
-                positionLong,
-                chargingSlots
-            },
-        endStation {
-            _id,
-            stationType,
-            positionLat,
-            positionLong,
-            chargingSlots
-        },
+            before,
+            after,
         }
     }
-}
-`
+}}
+`;
+
+export const deleteBooking = gql`
+  mutation ($id: ID!) {
+    deleteBooking(id: $id) {
+      _id
+      startDate
+      endDate
+      distance
+      incentiveUsed
+      startPosition {
+        lat
+        long
+      }
+      car {
+        carMode
+        name
+        carType
+        _id
+        lastTimeParked
+        parkedAt {
+          _id
+          stationType
+          positionLong
+          positionLat
+        }
+        currentRange
+        maximumRange
+      }
+      waypoints {
+        data {
+          positionLat
+          positionLong
+        }
+        before
+        after
+      }
+    }
+  }
+`;
+
+export const findBookings = gql`
+  query {
+    allBookings {
+      data {
+        _id
+        startDate
+        endDate
+        distance
+        car {
+          carMode
+          name
+          carType
+          _id
+          lastTimeParked
+          parkedAt {
+            _id
+            stationType
+            positionLong
+            positionLat
+          }
+          currentRange
+          maximumRange
+        }
+        incentiveUsed
+        startPosition {
+          lat
+          long
+        }
+        waypoints {
+          data {
+            positionLat
+            positionLong
+          }
+          before
+          after
+        }
+      }
+    }
+  }
+`;
 
 export const findBookingById = gql`
-  query($id: ID!) {
+  query ($id: ID!) {
+    query($id: ID!) {
     findBookingByID(id: $id) {
         _id,
         startDate,
@@ -184,20 +174,18 @@ export const findBookingById = gql`
             currentRange,
             maximumRange
         },
-        startStation {
-                _id,
-                stationType,
-                positionLat,
-                positionLong,
-                chargingSlots
-            },
-        endStation {
-            _id,
-            stationType,
-            positionLat,
-            positionLong,
-            chargingSlots
+        incentiveUsed,
+        startPosition {
+            lat,
+            long
         },
+        waypoints {
+            data {
+                positionLat,
+                positionLong
+            }
+        }
     }
-}
-`
+    }
+  }
+`;
