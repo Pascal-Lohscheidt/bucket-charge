f56bb2e73e48898ddea88ccd0f5c47599378dd34
Manuel Klettner <manuel.klettner@tum.de>
2022-11-20 07:02:16 +0100

    fix: return long distance in case of errors

--- Backend/src/services/getDistanceService.ts
+++ Backend/src/services/getDistanceService.ts
@@ -1,9 +1,12 @@
 import axios from "axios"
 
 export async function getDistance(startLat: string, startLong: string, endLat: string, endLong: string) {
+	try {
+		const { data }: any = await axios.get(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLat},${startLong}&destination=${endLat},${endLong}&units=metric&key=AIzaSyCylOoTb8pEjHGRPhUK2BNIfyxAOzf2cK8`)
 
-	const { data }: any = await axios.get(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLat},${startLong}&destination=${endLat},${endLong}&units=metric&key=AIzaSyCylOoTb8pEjHGRPhUK2BNIfyxAOzf2cK8`)
-
-	const distanceInMeters = data.routes[0].legs[0].distance.value
-	return distanceInMeters / 1000
+		const distanceInMeters = data.routes[0].legs[0].distance.value
+		return distanceInMeters / 1000
+	} catch {
+		return 9999
+	}
 }
